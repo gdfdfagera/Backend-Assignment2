5ef6ab15c91181153eb43f2fd8b4e59ffc5dd2cd
Diga <dias.gainatulinsc@gmail.com>
2024-01-19 13:50:18 +0600

    1

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const countries = require("i18n-iso-countries");
+const PORT = 3000;
+
+const app = express();
+
+const OPEN_WEATHER_API_KEY = '65c9fec7eae7d186800771c006793ad0';
+
+app.use('/public', express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/view/index.html");
+});
+
+app.post('/weather', async (req, res) => {
+  try {
+      let { city, country } = req.body;
+      const countryCode = getCountryCode(country).toLowerCase();
+      let weatherResponse = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${OPEN_WEATHER_API_KEY}`
+      );
+
+      let weatherData = {
+          temperature: kelvinToCelsius(weatherResponse.data.main.temp),
+          description: weatherResponse.data.weather[0].description,
+          icon: weatherResponse.data.weather[0].icon,
+          coordinates: {
+              latitude: weatherResponse.data.coord.lat,
+              longitude: weatherResponse.data.coord.lon,
+          },
+          feelsLike: kelvinToCelsius(weatherResponse.data.main.feels_like),
+          humidity: weatherResponse.data.main.humidity,
+          pressure: weatherResponse.data.main.pressure,
+          windSpeed: weatherResponse.data.wind.speed,
+          countryCode: weatherResponse.data.sys.country,
+          rainVolume: weatherResponse.data.rain ? weatherResponse.data.rain['1h'] : 0,
+      };
+
+      console.log(weatherData);
+
+      res.json(weatherData);
+  } catch (error) {
+      console.error('Error fetching weather data:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+function getCountryCode(countryName) {
+  const countryCode = countries.getAlpha2Code(countryName, "en");
+  return countryCode || "Не найден";
+}
+
+app.listen(PORT, () => {
+    console.log(`App is running on port: ${PORT}`);
+});
+
+function kelvinToCelsius(kelvin) {
+    return Math.floor(kelvin - 273.15);
+}
---
+++ public/js/index.js
@@ -0,0 +1,118 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('weather');
+    const input = document.getElementById('city');
+    const resultContainer = document.getElementById('weather-data');
+    const city_time = document.getElementsByClassName('city_time')[0];
+    const temperature = document.getElementsByClassName('temperature')[0];
+    const wind = document.getElementsByClassName('card_wind')[0];
+    const countryCode = document.getElementsByClassName('card_countryCode')[0];
+    const volume = document.getElementsByClassName('card_volume')[0];
+    const humanity = document.getElementsByClassName('card_humanity')[0];
+    const pressure = document.getElementsByClassName('card_pressure')[0];
+  
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault();
+  
+      let city = document.getElementById('city').value;
+      let country = document.getElementById('country').value;
+      var imagePath = '/public/image/';
+  
+      try {
+        let response = await axios.post(`/weather`, { city, country });
+        let data = response.data;
+        console.log(data);
+        var imageUrl = imagePath + data.icon + '@2x.png';
+        
+
+        city_time.innerHTML = `
+            <div>
+                <h2>${city}</h2>
+            </div>
+        `;
+
+        temperature.innerHTML = `
+            <div class="Celci">
+                <h3>${data.temperature} °C</h3>
+            </div>
+            <div class="description">
+                ${data.description}
+            </div>
+            <div class="icon">
+
+            </div>
+            <div class="feels">
+                Feels like ${data.feelsLike} °C
+            </div>
+        `;
+
+        wind.innerHTML = `
+            <div class="wind">
+                <b>Wind</b> <br>${data.windSpeed} m/s
+            </div>
+        `;
+
+        humanity.innerHTML = `
+            <div class="humanity">
+                <b>Humidity</b> <br>${data.humidity} %
+            </div>
+        `;
+
+        volume.innerHTML = `
+            <div class="volume">
+                <b>Rain volume</b> <br>${data.rainVolume} mm
+            </div>
+        `;
+
+        pressure.innerHTML = `
+            <div class="pressure"><b>Pressure</b> <br> ${data.pressure} hPa</div>
+        `;
+
+        countryCode.innerHTML = `
+            <div class="countryCode"><b>Country Code</b> <br>${data.countryCode}</div>
+        `;
+        
+        const imageContainer = document.getElementById('imageContainer');
+        var img = imageContainer.querySelector('img');
+        if (img) {
+          img.src = imageUrl;
+        } else {
+          img = new Image();
+          img.src = imageUrl;
+          img.onerror = function () {
+              console.error('Изображение не найдено');
+          };
+          imageContainer.appendChild(img);
+        }
+
+        img.onerror = function() {
+          console.error('Изображение не найдено');
+        };
+
+        google.maps.event.addDomListener(window, 'load', init(data.coordinates.latitude, data.coordinates.longitude));
+        
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        resultContainer.innerHTML = '<p>Error fetching weather data</p>';
+      }
+    });
+});
+
+
+
+function init(lat, lng) {
+    let mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(lat, lng),
+        styles: [{"featureType":"all","elementType":"labels.text","stylers":[{"color":"#878787"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f9f5ed"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"color":"#f5f5f5"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#aee0f4"}]}]
+    };
+  
+    let mapElement = document.getElementById('map');
+  
+    let map = new google.maps.Map(mapElement, mapOptions);
+  
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map,
+        title: 'Snazzy!'
+    });
+  }
